820ac6379977b2d65cce1a240c87d489852a5e9c
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:42:59 +0530

    feat(auth):add admin-login module

---
+++ modules/admin/components/Admin-signup.tsx
@@ -0,0 +1,11 @@
+import { Login } from "@/shared/components/reusables/Login"
+
+const AdminLogin = () => {
+    return (
+        <>
+        <Login role={'ADMIN'}/>
+        </>
+    )
+}
+
+export default AdminLogin
