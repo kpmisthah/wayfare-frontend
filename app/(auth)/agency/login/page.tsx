33534697ed44ed20bc56fb477a5d2a74707328ed
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:05:53 +0530

    feat(auth):add agency-login  page that render AgencyLogin component

---
+++ app/(auth)/agency/login/page.tsx
@@ -0,0 +1,11 @@
+import { AgencyLogin } from "@/modules/agency/components/Agency-login";
+
+const page = ()=>{
+    return(
+        <>
+        <AgencyLogin />
+        </>
+    )
+}
+
+export default page
