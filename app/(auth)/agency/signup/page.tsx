50e588b827b76d44ec2439794fec91014e11b808
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:07:15 +0530

    feat(auth):add agency-signup page that render AgencySginup component

---
+++ app/(auth)/agency/signup/page.tsx
@@ -0,0 +1,12 @@
+import AgencySignup from '@/modules/agency/components/Agency-signup'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <AgencySignup />
+    </>
+  )
+}
+
+export default page
