b596e77788422e86dc29eeddf07c4a6e2036f474
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 17:36:10 +0530

    feat(admin/user): add hook to manage user edit form logic

---
+++ modules/admin/hooks/user-management/use-edit-form.ts
@@ -0,0 +1,35 @@
+"use client"
+import { useEffect, useState } from "react";
+import { User } from "../../types/user.type";
+export const useEditForm = (user?:User)=>{
+    const [formData, setFormData] = useState<Partial<User>>({
+    name: user?.name || '',
+    profile: user?.profile || '',
+    email: user?.email || '',
+    isBlock: user?.isBlock || false
+  });
+  
+  const handleChange = (field:string, value:string|boolean) => {
+    setFormData(prev => ({
+      ...prev,
+      [field]: value
+    }));
+  };
+
+  // Reset form when user changes
+    useEffect(() => {
+    if (user) {
+      setFormData({
+        name: user.name,
+        profile: user.profile,
+        email: user.email,
+        isBlock: user.isBlock
+      });
+    }
+  }, [user]);
+    return {
+        formData,
+        setFormData,
+        handleChange
+    }
+}
