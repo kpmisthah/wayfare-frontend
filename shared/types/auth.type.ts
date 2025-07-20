2aaf414f437564c17ac77152ec4ff88f8e9132c7
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:33:05 +0530

    feat(auth): add type for auth

---
+++ shared/types/auth.type.ts
@@ -0,0 +1,54 @@
+export interface SignupForm  {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  agreeToTerms: boolean;
+  mobile?:string
+  role?:string 
+};
+export interface LoginForm {
+  email:string,
+  password:string
+}
+export interface ErrorMessages {
+  [key: string]: string; 
+};
+
+export interface AuthLayoutProps {
+  children: React.ReactNode;
+};
+export interface AuthTokens {
+  accessToken: string;
+
+}
+
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+  profileImage?:string
+
+}
+
+export interface AuthResponse {
+  user:User
+}
+
+export interface AuthError {
+  type:string
+  field?:string
+}
+
+export interface UseSignupProps {
+    role?:"USER"|'AGENCY'|'ADMIN'
+}
+
+// export interface SignupProps {
+//   role?: string;
+//   signupData: SignupForm;
+//   setSignupData: React.Dispatch<React.SetStateAction<SignupForm>>;
+//   errors: ErrorMessages;
+//   isLoading: boolean;
+//   onSubmit: () => void;
+// };
