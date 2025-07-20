8611f21c815ba270b53ad5be2e2cddfb3146abfa
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:44:36 +0530

    feat(auth):add agency-signup module

---
+++ modules/agency/components/Agency-signup.tsx
@@ -0,0 +1,12 @@
+import { Signup } from '@/shared/components/reusables/Register'
+import React from 'react'
+
+const AgencySignup = () => {
+  return (
+    <>
+    <Signup role={'AGENCY'}/>
+    </>
+  )
+}
+
+export default AgencySignup
