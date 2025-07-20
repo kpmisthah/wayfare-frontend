a03e9ca3767c8f6026ebd1a021c65a3c50233af7
kpmisthah <kpmisthah6@gmail.com>
2025-07-20 16:01:49 +0530

    feat(auth):add reset-password  page that render ResetPasswordPage  component

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordPage from "@/modules/auth/components/Reset-password"
+
+export default function NewPassword(){
+    return(
+        <>
+        <ResetPasswordPage />
+        </>
+    )
+}
