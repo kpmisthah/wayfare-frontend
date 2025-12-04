c5d3df33f5e19d1c1e2a05fcaa0b6dbd346de210
kpmisthah <kpmisthah6@gmail.com>
2025-12-04 16:59:38 +0530

    feat(admin):Implmeent pagination in agency management

--- modules/admin/services/agency.service.ts
+++ modules/admin/services/agency.service.ts
@@ -1,40 +1,34 @@
 import api from "@/lib/api";
-export const getAgencies = async ()=>{
-    try {
-        const response = await api.get('/admin/agencies',{
-            // params:{page,limit,search}
-        })
-        return response.data
-    } catch (error) {
-        console.log(error);
-        throw error
-        
-    }
-}
+export const getAgencies = async ({ page = 1, limit = 10, search = "" }) => {
+  try {
+    const response = await api.get(`/admin/agencies?page=${page}&limit=${limit}&search=${search}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-export const approvalAgencies = async (id:string,action:string,reason?:string) =>{
-    try {
-        const response = await api.patch(`/agency/${id}`,{action,reason})
-        return response.data
-    } catch (error) {
-        console.log(error);
-        throw error
-        
-    }
-}
+export const approvalAgencies = async (
+  id: string,
+  action: string,
+  reason?: string
+) => {
+  try {
+    const response = await api.patch(`/agency/${id}`, { action, reason });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-
-
-export const updateAgencies = async (agencyId:string) =>{
-    try {
-        const response = await api.patch(`/agency/profile/${agencyId}`)
-        return response.data
-    } catch (error) {
-        console.log(error);
-        throw error
-        
-    }
-}
-
-
-
+export const updateAgencies = async (agencyId: string) => {
+  try {
+    const response = await api.patch(`/agency/profile/${agencyId}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
