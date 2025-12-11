517dd1b48d5656551dd522d1307da4a85ff90230
kpmisthah <kpmisthah6@gmail.com>
2025-12-11 23:59:11 +0530

    fix:(user):add more types in user interface

--- modules/admin/types/user.type.ts
+++ modules/admin/types/user.type.ts
@@ -1,16 +1,18 @@
-export interface User {
-  profileImage: string | undefined;
-  bannerImage: string|undefined;
+export interface PreferenceTag {
   id: string;
-  location:string
   name: string;
-  phone:string
-  email: string;
-  isBlock: boolean; 
-  isVerified:boolean
 }
 
-interface PreferenceTag {
-  id:string;
-  name:string
+export interface User {
+  profileImage: string | undefined;
+  bannerImage: string | undefined;
+  id: string;
+  location: string;
+  name: string;
+  phone: string;
+  email: string;
+  isBlock: boolean;
+  isVerified: boolean;
+  role?: string;
+  preference?: PreferenceTag[];
 }
