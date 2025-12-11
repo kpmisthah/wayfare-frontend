9a4314bbbd8fc67f5aa148905ccfdd623793bacf
kpmisthah <kpmisthah6@gmail.com>
2025-12-12 00:28:48 +0530

    feat:(search):implement search in packages

--- modules/agency/hooks/use-fetch-packages.ts
+++ modules/agency/hooks/use-fetch-packages.ts
@@ -2,32 +2,35 @@
 import { useEffect, useState } from "react";
 import { fetchPackages, updatedPackage, updatePackageStatus } from "../services/package.api";
 import { PackageData, PackageListing } from "../types/package.type";
-import { PackageStatus } from "../types/package.enum";
+
 
 export const useFetchPackages = () => {
   const [packages, setPackages] = useState<PackageData[]>([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
+  const [search, setSearch] = useState(''); 
   const limit = 6;
 
   useEffect(() => {
     setLoading(true)
     try {
       const loadPackages = async () => {
-        const data = await fetchPackages(page, limit);
-        // console.log(data.items,'in fetch packagess')
+        const data = await fetchPackages(page, limit, search); 
         setPage(data.page);
-        setPackages(data.items||[]);
+        setPackages(data.items || []);
         setTotalPage(data.totalPages);
-        setTotalPage(data.total);
       };
-      loadPackages();
+      const timeoutId = setTimeout(() => { 
+        loadPackages();
+      }, 500);
+      return () => clearTimeout(timeoutId);
     } catch (error) {
     } finally {
-        setLoading(false)
+      setLoading(false)
     }
-  }, [page, limit]);
+  }, [page, limit, search]);
+
   const nextPage = () => page < totalPage && setPage((prev) => prev + 1);
   const prevPage = () => page > 1 && setPage((prev) => prev - 1);
 
@@ -41,6 +44,8 @@ export const useFetchPackages = () => {
     page,
     totalPage,
     loading,
-    setLoading
+    setLoading,
+    search,
+    setSearch
   };
 };
