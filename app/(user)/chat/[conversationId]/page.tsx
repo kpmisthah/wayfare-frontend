d6d6442726f7e04afab13e642a8df6bda467e3ff
kpmisthah <kpmisthah6@gmail.com>
2025-12-15 21:25:13 +0530

    fix:(coverstationId):conversation id issue during build

--- app/(user)/chat/[conversationId]/page.tsx
+++ app/(user)/chat/[conversationId]/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { use } from "react";
 import Chat from "@/modules/user/components/chat/chat";
 import { withAuth } from "@/shared/components/HOC/withAuth";
 import { useAuthStore } from "@/store/Auth";
 
-function ChatPage({ params }: { params: { conversationId: string } }) {
+function ChatPage({ params }: { params: Promise<{ conversationId: string }> }) {
   const { user } = useAuthStore();
-  const conversationId = params.conversationId;
+  const { conversationId } = use(params);
 
-  if (!user) return null; 
+  if (!user) return null;
 
   return (
     <Chat
