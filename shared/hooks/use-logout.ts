484146a621eb2dbb91c53d8e5fead3cc97a37fcf
kpmisthah <kpmisthah6@gmail.com>
2025-12-19 09:41:44 +0530

    fix:(master):fix logout issue

--- shared/hooks/use-logout.ts
+++ shared/hooks/use-logout.ts
@@ -1,23 +1,23 @@
 import { useRouter } from "next/navigation";
 import { logout } from "@/shared/services/auth.api";
 import { useAuthStore } from "@/store/Auth";
-export const useLogout = ()=>{
-    const {clearAuth} = useAuthStore()
-    const router = useRouter()
+export const useLogout = () => {
+  const { clearAuth } = useAuthStore()
+  const router = useRouter()
 
-    const handleLogin = () => {
-        router.push('/login')
-    };
-    
-    const handleLogout = async () => {
-     const response = await logout()
-     let role = response?.role
-     console.log(role,'==============frpm use-logout=============')
+  const handleLogin = () => {
+    router.push('/login')
+  };
+
+  const handleLogout = async () => {
+    const response = await logout()
+    let role = response?.role
+    console.log(role, '==============frpm use-logout=============')
     clearAuth()
-    if(role == 'ADMIN'){
+    if (role == 'ADMIN') {
       router.push("/admin/login")
     }
-    if(role == 'AGENCY'){
+    if (role == 'AGENCY') {
       router.push('/agency/login')
     }
   };
@@ -30,5 +30,5 @@ export const useLogout = ()=>{
     handleLogin,
     handleLogout,
     handleSignUp
-}
+  }
 }
