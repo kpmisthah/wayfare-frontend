480e597543b440ce04ed40a9d518f404aa062952
kpmisthah <kpmisthah6@gmail.com>
2025-12-21 01:26:18 +0530

    feat(user):implment appropriate changes in hook for implmeneting modal

--- modules/admin/hooks/user-management/use-user-management.ts
+++ modules/admin/hooks/user-management/use-user-management.ts
@@ -5,54 +5,58 @@ import { getUser, updateUser, deleteUser } from "../../services/user.service";
 export const useUsers = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [page, setPage] = useState<number>(1);
-  const [totalPage,setTotalPage] = useState(1)
+  const [totalPage, setTotalPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false);
   const limit = 3;
   const [searchTerm, setSearchTerm] = useState<string>("");
-  
+
   const [editingUser, setEditingUser] = useState<User>();
   const [editDialogOpen, setEditDialogOpen] = useState(false);
   const [blockModalOpen, setBlockModalOpen] = useState(false);
   const [userToBlock, setUserToBlock] = useState<User | null>(null);
 
-  const handleEditUser = (user:User) => {
+  const handleEditUser = (user: User) => {
     setEditingUser(user);
     setEditDialogOpen(true);
   };
 
-  const handleSaveUser = async (updatedData:Partial<User>) => {
+  const handleSaveUser = async (updatedData: Partial<User>) => {
     try {
       await update(editingUser?.id!, updatedData);
-      setUsers((prev)=>
-        prev.map((user)=>
-          user.id == editingUser?.id?{...user,...updatedData}:user
+      setUsers((prev) =>
+        prev.map((user) =>
+          user.id == editingUser?.id ? { ...user, ...updatedData } : user
         )
       )
     } catch (error) {
-      
+
     }
   };
-  const handleToggleBlockUser = async(user:User) => {
+  const handleToggleBlockUser = async (user: User) => {
     const updateStatus = !user.isBlock
-    console.log(updateStatus,'updatestatus')
+    console.log(updateStatus, 'updatestatus')
     try {
-      await update(user.id,{isBlock:updateStatus})
-      setUsers((prev)=>
-        prev.map((u)=>
-          u.id == user.id?{...u,isBlock:updateStatus}:u
+      await update(user.id, { isBlock: updateStatus })
+      setUsers((prev) =>
+        prev.map((u) =>
+          u.id == user.id ? { ...u, isBlock: updateStatus } : u
         )
       )
     } catch (error) {
-       console.error("Failed to update user block status", error);
+      console.error("Failed to update user block status", error);
     }
   };
 
   const fetchUser = async () => {
+    setIsLoading(true);
     try {
       const res = await getUser(page, limit, searchTerm);
       setUsers(res.data);
       setTotalPage(res.totalPages)
     } catch (error) {
       console.error("Failed to fetch users:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -60,14 +64,14 @@ export const useUsers = () => {
     fetchUser();
   }, [page, searchTerm]);
   useEffect(() => {
-  setPage(1); 
-}, [searchTerm]);
+    setPage(1);
+  }, [searchTerm]);
 
   const update = async (id: string, updateData: Partial<User>) => {
     try {
-      console.log(updateData,'update data in hook')
+      console.log(updateData, 'update data in hook')
       let result = await updateUser(id, updateData);
-      console.log(result,'updated user result')
+      console.log(result, 'updated user result')
       setUsers((prevUser) =>
         prevUser.map((user) =>
           user.id == id ? { ...updateData, ...user } : user
@@ -86,7 +90,7 @@ export const useUsers = () => {
           user.id == id ? { ...user, ...updateData } : user
         )
       );
-    } catch (error) {}
+    } catch (error) { }
   };
 
   return {
@@ -111,5 +115,6 @@ export const useUsers = () => {
     setBlockModalOpen,
     userToBlock,
     setUserToBlock,
+    isLoading,
   };
 };
